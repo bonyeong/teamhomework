13816bd1a0a2d414c2054de8bcecb4ff71dd4b36
Lee-Ha-Eun-00 <63087285+Lee-Ha-Eun-00@users.noreply.github.com>
2020-04-26 22:35:25 +0900

    project1(ha-eun)

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+import telegram
+import sys
+import ChatBotModel
+
+chii_token = '1184525831:AAGXQ9-Es5COmMc8TSjV3Z6EWGHy3efjyUI'
+chii = telegram.Bot(token = chii_token)
+updates = chii .getUpdates()
+for u in updates:
+    print(u.message)
+
+def proc_rolling(bot, update):
+    chii.sendMessage('안녕')
+    chii.sendMessage('만나서 반가워. 나는 챗봇이야')
+    chii.sendMessage('무엇을 도와줄까?')
+
+def proc_stop(bot, update):
+    chii.sendMessage('치이 봇이 잠듭니다.')
+    chii.stop()
+
+chii = ChatBotModel.BotChii()
+chii.add_handler('hi', proc_rolling)
+chii.add_handler('stop', proc_stop)
+chii.start()
